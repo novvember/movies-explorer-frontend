2baefdc709861f4e2189d679e457fd13464e9721
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-07 21:56:35 +0300

    content: add links to portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,18 +6,44 @@ function Portfolio() {
       <h3 className="portfolio__title">Портфолио</h3>
       <ul className="portfolio__list">
         <li className="portfolio__list-item">
-          <a href="#" className="portfolio__link" target="_blank">
-            Статичный сайт
+          <a
+            href="https://novvember.github.io/lubimovka-landing/"
+            className="portfolio__link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Лендинг фестиваля &laquo;Любимовка&raquo;
           </a>
         </li>
         <li className="portfolio__list-item">
-          <a href="#" className="portfolio__link" target="_blank">
-            Адаптивный сайт
+          <a
+            href="https://github.com/RomanShigapov/ITMO/tree/develop"
+            className="portfolio__link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Лендинг для лаборатории ИТМО
           </a>
         </li>
         <li className="portfolio__list-item">
-          <a href="#" className="portfolio__link" target="_blank">
-            Одностраничное приложение
+          <a
+            href="https://mesto.novvember.nomoredomains.sbs/"
+            className="portfolio__link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Приложение с&nbsp;фотографиями и&nbsp;лайками Mesto
+          </a>
+        </li>
+        <li className="portfolio__list-item">
+          <a
+            href="https://novvember.github.io/yet-another-sorting-visualization"
+            className="portfolio__link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Приложение с&nbsp;визуализацией алгоритмов сортировки Yet Another
+            Sorting Visualization
           </a>
         </li>
       </ul>
