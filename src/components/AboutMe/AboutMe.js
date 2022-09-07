b68ed2f74c20c49338209f34c1e26cc62f8956d2
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-07 19:05:55 +0300

    feat: add personal info about student

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,7 +1,52 @@
 import './AboutMe.css';
+import photo from '../../images/student-photo.jpg';
 
 function AboutMe() {
-  return <article className="about-me">AboutMe</article>;
+  return (
+    <article className="about-me">
+      <img src={photo} alt="Фотография Дмитрия" className="about-me__photo" />
+      <p className="about-me__name">Дмитрий</p>
+      <p className="about-me__subtitle">Фронтенд-разработчик, 34&nbsp;года</p>
+      <p className="about-me__description">
+        Нравится продумывать логику, структуру, а&nbsp;потом реализовывать
+        понятные для пользователей интерфейсы, а&nbsp;также учитывать требования
+        стандартов по&nbsp;семантике и&nbsp;доступности. Хочу продолжать изучать
+        и&nbsp;использовать современные веб-технологии, приемы и&nbsp;софт
+        и&nbsp;развиваться в&nbsp;этом направлении.
+      </p>
+      <p className="about-me__description">
+        Стараюсь постоянно изучать новое: делаю дополнительные проекты
+        на&nbsp;курсах, помогаю на&nbsp;Stackoverflow, решаю задачки
+        на&nbsp;Codewars. Читаю дополнительные источники, мои любимые:
+        learn.javascript.ru (куда без него), регулярные новости фронтенда
+        на&nbsp;Хабре от&nbsp;HTML&nbsp;Academy, классные статьи от&nbsp;CSS
+        Tricks или Josh Comeau.
+      </p>
+      <ul className="about-me__link-list">
+        <li className="about-me__link-list-item">
+          <a href="https://github.com/novvember" className="about-me__link">
+            Github
+          </a>
+        </li>
+        <li className="about-me__link-list-item">
+          <a
+            href="https://ru.stackoverflow.com/users/352251"
+            className="about-me__link"
+          >
+            Stackoverflow
+          </a>
+        </li>
+        <li className="about-me__link-list-item">
+          <a
+            href="https://www.codewars.com/users/novvember"
+            className="about-me__link"
+          >
+            Codewars
+          </a>
+        </li>
+      </ul>
+    </article>
+  );
 }
 
 export default AboutMe;
