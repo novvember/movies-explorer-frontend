61f3cef9abc9a17ea7bf6e3ff47e160fcfdc7ea2
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-08 16:50:33 +0300

    feat: add nNavigation component (version with slider)

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,67 @@
+import React from 'react';
 import './Navigation.css';
 
 function Navigation() {
-  return 'navigation';
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  function toggleMenu() {
+    setIsOpen((state) => !state);
+  }
+
+  return (
+    <div className="navigation">
+      <button
+        type="button"
+        className="navigation__open-button"
+        aria-label="Открыть меню"
+        onClick={toggleMenu}
+      ></button>
+
+      <div
+        className={`navigation__menu${
+          isOpen ? ' navigation__menu_opened' : ''
+        }`}
+      >
+        <nav className="navigation__panel">
+          <button
+            type="button"
+            className="navigation__close-button"
+            aria-label="Закрыть меню"
+            onClick={toggleMenu}
+          ></button>
+
+          <ul className="navigation__list">
+            <li className="navigation__list-item">
+              <a className="navigation__link" href="#test">
+                Главная
+              </a>
+            </li>
+            <li className="navigation__list-item">
+              <a
+                className="navigation__link navigation__link_active"
+                href="#test"
+              >
+                Фильмы
+              </a>
+            </li>
+            <li className="navigation__list-item">
+              <a className="navigation__link" href="#test">
+                Сохранённые фильмы
+              </a>
+            </li>
+            <li className="navigation__list-item">
+              <a
+                className="navigation__link navigation__link_type_account"
+                href="#test"
+              >
+                Аккаунт
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
