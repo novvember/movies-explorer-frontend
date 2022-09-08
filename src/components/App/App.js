61c8e3e692534c51b8961679aa03ef39ac87e595
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-08 20:55:14 +0300

    feat: add route for movies page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Landing from '../Landing/Landing';
+import Movies from '../Movies/Movies';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="content">
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/movies" element={<Movies />} />
       </Routes>
     </div>
   );
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,20 @@
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Navigation from '../Navigation/Navigation';
+import './Movies.css';
+
+function Movies() {
+  return (
+    <>
+      <Header>
+        <Navigation />
+      </Header>
+      <main>
+        Содержимое
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Movies;
