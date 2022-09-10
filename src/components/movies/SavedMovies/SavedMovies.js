a355e596e9660354612b0cc5030598347c9ff6c9
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-11 00:07:42 +0300

    feat: add SavedMovies component

---
+++ src/components/movies/SavedMovies/SavedMovies.js
@@ -0,0 +1,23 @@
+import Footer from '../../common/Footer/Footer';
+import Header from '../../common/Header/Header';
+import Navigation from '../../common/Navigation/Navigation';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import SearchForm from '../SearchForm/SearchForm';
+import './SavedMovies.css';
+
+function SavedMovies() {
+  return (
+    <>
+      <Header>
+        <Navigation />
+      </Header>
+      <main>
+        <SearchForm />
+        <MoviesCardList />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default SavedMovies;
