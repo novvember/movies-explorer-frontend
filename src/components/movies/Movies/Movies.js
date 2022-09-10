9ce853049a392baf3274eb279fb50594137e5d93
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-10 23:58:34 +0300

    feat: add More component

--- src/components/movies/Movies/Movies.js
+++ src/components/movies/Movies/Movies.js
@@ -1,6 +1,7 @@
 import Footer from '../../common/Footer/Footer';
 import Header from '../../common/Header/Header';
 import Navigation from '../../common/Navigation/Navigation';
+import More from '../More/More';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SearchForm from '../SearchForm/SearchForm';
 import './Movies.css';
@@ -14,6 +15,7 @@ function Movies() {
       <main>
         <SearchForm />
         <MoviesCardList />
+        <More />
       </main>
       <Footer />
     </>
