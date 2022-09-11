2034660fa6b93ff07079682617e460b594604f44
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-11 18:20:54 +0300

    feat: add Auth component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import ProfilePage from '../ProfilePage/ProfilePage';
 import SavedMovies from '../movies/SavedMovies/SavedMovies';
 import Page404 from '../Page404/Page404';
 import './App.css';
+import Auth from '../auth/Auth/Auth';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path="/movies" element={<Movies />} />
         <Route path="/saved-movies" element={<SavedMovies />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/signup" element={<Auth />} />
+        <Route path="/signin" element={<Auth />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
---
+++ src/components/auth/Auth/Auth.js
@@ -0,0 +1,71 @@
+import './Auth.css';
+import logo from '../../../images/logo.svg';
+import { Link } from 'react-router-dom';
+
+function Auth({ mode = 'register' }) {
+  const MODES = {
+    register: {
+      title: 'Добро пожаловать!',
+      hint: (
+        <p className="auth__hint">
+          Уже зарегистрированы?{' '}
+          <Link to="/signin" className="auth__hint-link">
+            Войти
+          </Link>
+        </p>
+      ),
+    },
+    login: {
+      title: 'Рады видеть!',
+      hint: (
+        <p className="auth__hint">
+          Ещё не зарегистрированы?{' '}
+          <Link to="/signup" className="auth__hint-link">
+            Регистрация
+          </Link>
+        </p>
+      ),
+    },
+  };
+
+  return (
+    <main className="auth content__stretched-element">
+      <div className="auth__container">
+        <Link className="auth__logo-link" to="/">
+          <img
+            className="header__logo"
+            alt="Логотип приложения: круг"
+            src={logo}
+          />
+        </Link>
+        <h1 className="auth__title">{MODES[mode].title}</h1>
+        <form className="auth__form">
+          <label className="auth__input-container">
+            <span className="auth__label">Имя</span>
+            <input type="text" className="auth__input" name="name" required />
+          </label>
+          <label className="auth__input-container">
+            <span className="auth__label">E-mail</span>
+            <input type="email" className="auth__input" name="email" required />
+          </label>
+          <label className="auth__input-container">
+            <span className="auth__label">Пароль</span>
+            <input
+              type="password"
+              className="auth__input"
+              name="password"
+              required
+            />
+          </label>
+          <p className="auth__error">Что-то пошло не так...</p>
+          <button className="auth__submit-button" type="submit">
+            Зарегистрироваться
+          </button>
+        </form>
+        {MODES[mode].hint}
+      </div>
+    </main>
+  );
+}
+
+export default Auth;
