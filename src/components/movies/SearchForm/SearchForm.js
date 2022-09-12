fc6168810b3d48e6a4b5fede204849aaa932e284
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-12 20:20:48 +0300

    fix: add required attribute to movie search field

--- src/components/movies/SearchForm/SearchForm.js
+++ src/components/movies/SearchForm/SearchForm.js
@@ -4,7 +4,12 @@ import './SearchForm.css';
 function SearchForm() {
   return (
     <form className="search-form section">
-      <input type="text" className="search-form__input" placeholder="Фильм" />
+      <input
+        type="text"
+        className="search-form__input"
+        placeholder="Фильм"
+        required
+      />
       <Checkbox title="Короткометражки" className="search-form__checkbox" />
       <button type="submit" className="search-form__button">
         Найти
