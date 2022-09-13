debee5dda304788cd75290eb2618c063af6dc12c
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-13 14:17:51 +0300

    feat: add controled inputs and validation to search form

--- src/components/movies/Checkbox/Checkbox.js
+++ src/components/movies/Checkbox/Checkbox.js
@@ -1,13 +1,16 @@
 import classNames from 'classnames';
 import './Checkbox.css';
 
-function Checkbox({ className, title }) {
+function Checkbox({ className, title, name, value, onChange }) {
   return (
     <label className={classNames(className, 'checkbox')}>
       <input
         type="checkbox"
         className="checkbox__system-checkbox"
         defaultChecked={true}
+        name={name}
+        checked={value}
+        onChange={onChange}
       />
       <span className="checkbox__custom-checkbox"></span>
       <span className="checkbox__label">{title}</span>
--- src/components/movies/SearchForm/SearchForm.js
+++ src/components/movies/SearchForm/SearchForm.js
@@ -1,16 +1,62 @@
+import React from 'react';
 import Checkbox from '../Checkbox/Checkbox';
 import './SearchForm.css';
 
-function SearchForm() {
+function SearchForm({ onSubmit }) {
+  const DEFAULT_VALUES = { searchText: '', showShorties: true };
+  const [inputValues, setInputValues] = React.useState(DEFAULT_VALUES);
+  const [errorText, setErrorText] = React.useState('');
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value =
+      event.target.type === 'checkbox'
+        ? event.target.checked
+        : event.target.value;
+    setInputValues((state) => ({ ...state, [name]: value }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!validateForm(event.target)) return;
+    onSubmit();
+  }
+
+  function validateForm(form) {
+    const isValid = form.checkValidity();
+
+    if (!isValid) {
+      setError('Нужно ввести ключевое слово');
+      return false;
+    }
+
+    setError('');
+    return true;
+  }
+
+  function setError(text) {
+    setErrorText(text);
+  }
+
   return (
-    <form className="search-form section">
+    <form className="search-form section" onSubmit={handleSubmit} noValidate>
       <input
         type="text"
         className="search-form__input"
         placeholder="Фильм"
+        name="searchText"
         required
+        value={inputValues.search}
+        onChange={handleChange}
       />
-      <Checkbox title="Короткометражки" className="search-form__checkbox" />
+      <Checkbox
+        title="Короткометражки"
+        className="search-form__checkbox"
+        name="showShorties"
+        value={inputValues.shorties}
+        onChange={handleChange}
+      />
+      <span className="search__error">{errorText}</span>
       <button type="submit" className="search-form__button">
         Найти
       </button>
