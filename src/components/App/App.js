a061d0690f5f0692d961a76173370b4daae2d747
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-09-15 12:21:04 +0300

    feat: add context for current user

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,20 +7,27 @@ import Page404 from '../Page404/Page404';
 import './App.css';
 import Register from '../user/Register/Register';
 import Login from '../user/Login/Login';
+import React from 'react';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
 
 function App() {
+  const [currentUser, setCurrentUser] = React.useState({});
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
   return (
-    <div className="content">
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/saved-movies" element={<SavedMovies />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/signin" element={<Login />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </div>
+    <CurrentUserContext.Provider user={currentUser}>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/saved-movies" element={<SavedMovies />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
